2dc575e0986a21fa81858b9a7ad533f7d60ebc05
emereson <escanorrosa123@gmail.com>
2023-11-27 20:55:09 -0500

    hola

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/projectsStyle.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Portfolio = () => {
+  const [projects, setprojects] = useState();
+
+  useEffect(() => {
+    const url = `${import.meta.env.VITE_URL_API}/project`;
+
+    axios
+      .get(url)
+      .then((res) => setprojects(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="portfolio__container">
       <section className="portfolio__section-one">
@@ -15,103 +28,24 @@ const Portfolio = () => {
       </section>
 
       <section className="portfolio__section-two">
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./logoHenry.png" alt="" />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Henry Gil Weddings </h3>
-            <p>
-              Diseñada para un talentoso camarógrafo profesional, esta página muestra su
-              impresionante trabajo en forma de videos de alta calidad y una variada galería de
-              imágenes capturadas en diferentes situaciones y lugares.
-            </p>
-
-            <a
-              href="https://henrygilweddings.com"
-              target="_blank"
-              className="portfolioProject__link"
-            >
-              ver pagina
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./logoPadel.png" alt="" />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Liga Padel</h3>
-            <p>
-              Liga Padel, mi primer proyecto como desarrollador freelance, permite a los usuarios
-              crear y participar en eventos de pádel, con funciones que incluyen inscripciones,
-              calendarios y detalles de jugadores.
-            </p>
-            <a
-              href="https://strong-pixie-20867c.netlify.app/"
-              target="_blank"
-              className="portfolioProject__link"
-            >
-              ver pagina
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./colegioLogo.png" alt="" style={{ width: '60%' }} />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Alipio Ponce </h3>
-            <p>
-              Sistema de control, permitiendo al director y profesores gestionar asistencias, notas,
-              observaciones, exámenes y mensualidades de los estudiantes de manera eficiente.
-            </p>
-          </div>
-        </article>
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./policlinico.png" alt="" />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Poclinico</h3>
-            <p>
-              Desarrollé esta página para un policlínico local. Los pacientes pueden revisar sus
-              ecografías y realizar consultas directamente a través de la plataforma web.
-            </p>
-          </div>
-        </article>
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./netflyx.png" alt="" />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Netflyx</h3>
-            <p>
-              Explora tráilers de películas y series en esta página personalizada. Busca y filtra
-              por nombre o género para encontrar contenido que te interese.
-            </p>
-            <a
-              href="https://dulcet-cucurucho-90d923.netlify.app/"
-              target="_blank"
-              className="portfolioProject__link"
-            >
-              ver pagina
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__project">
-          <div className="portfolioProject__img">
-            <img src="./qrPet.png" alt="" />
-          </div>
-          <div className="portfolioProject__data">
-            <h3>Aquí Estoy</h3>
-            <p>
-              Escanea el código QR en el collar de la mascota para acceder a su información y
-              ubicación actual a través de Google Maps. También puedes compartir la ubicación.
-            </p>
-          </div>
-        </article>
+        {projects?.projects.map((project) => (
+          <article className="portfolio__project" key={project.id}>
+            <div className="portfolioProject__img">
+              <img src={project.projectImg} alt="" />
+            </div>
+            <div className="portfolioProject__data">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              {project.linkPage === '1' ? (
+                <span></span>
+              ) : (
+                <a href={project.linkPage} target="_blank" className="portfolioProject__link">
+                  ver pagina
+                </a>
+              )}
+            </div>
+          </article>
+        ))}
       </section>
     </div>
   );
